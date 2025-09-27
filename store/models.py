6ee3f692636dde32ff485d197980233f4ad052dc
mishu-anik3 <anpaul098@gmail.com>
2025-09-27 11:09:53 +0200

    create Category class from inheritng Django MPTT model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='categories/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
